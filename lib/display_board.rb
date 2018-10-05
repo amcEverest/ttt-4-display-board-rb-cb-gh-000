1b776689489aef1b03d5671042519847c73d5acc
Andres Cortez <andres.corteztm@gmail.com>
2018-10-05 20:50:22 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,6 +6,17 @@ def display_board ()
   puts "   |   |   "
 end
 
-board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+display_board(board)
 
+board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+display_board(board)
+
+board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+display_board(board)
+
+board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
+display_board(board)
+
+board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
 display_board(board)
