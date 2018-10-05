3ef0ddd8347e800b36dc07f2ceee35111128c331
Andres Cortez <andres.corteztm@gmail.com>
2018-10-05 20:45:21 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,7 @@
-display_board()
-  puts "#{board[0]}|#{board[1]}|#{board[2]}"
+def display_board ()
+  puts "   |   |   "
   puts "-----------"
-  puts "#{board[3]}|#{board[4]}|#{board[5]}"
+  puts "   |   |   "
   puts "-----------"
-  puts "#{board[6]}|#{board[7]}|#{board[8]}"
+  puts "   |   |   "
 end
-
-board = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
-
-display_board (board)
